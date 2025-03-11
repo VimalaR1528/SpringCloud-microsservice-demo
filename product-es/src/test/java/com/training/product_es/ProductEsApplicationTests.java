40dae2bc188409d56087dc936483fb530775221a
Vimala <44087450+VimalaR1528@users.noreply.github.com>
2025-03-11 06:56:22 +0000

    Add files via upload

---
+++ product-es/src/test/java/com/training/product_es/ProductEsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.product_es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductEsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
