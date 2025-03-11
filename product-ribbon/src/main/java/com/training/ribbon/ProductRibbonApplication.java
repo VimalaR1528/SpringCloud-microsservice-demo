348b82a297378ab438647dc4c7950dbd137690a6
Vimala <44087450+VimalaR1528@users.noreply.github.com>
2025-03-11 06:58:42 +0000

    Add files via upload

---
+++ product-ribbon/src/main/java/com/training/ribbon/ProductRibbonApplication.java
@@ -0,0 +1,13 @@
+package com.training.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductRibbonApplication.class, args);
+	}
+
+}
---
+++ product-ribbon/src/main/java/com/training/ribbon/controller/ProductClientController.java
@@ -0,0 +1,25 @@
+package com.training.ribbon.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.training.ribbon.domain.Product;
+
+@RestController
+@Scope("request")
+public class ProductClientController {
+
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@GetMapping("/get-products/{id}")
+	public Product getProductById(@PathVariable("id")Integer id) {
+		Product product=restTemplate.getForObject("http://product-service/products/"+id, Product.class);
+		
+		return product;
+	}
+}
