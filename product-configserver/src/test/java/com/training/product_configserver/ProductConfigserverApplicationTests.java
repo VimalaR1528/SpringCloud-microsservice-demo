2c557c85f28b3f9f21da68cc32e8cc3787a93de4
Vimala <44087450+VimalaR1528@users.noreply.github.com>
2025-03-11 06:55:25 +0000

    Add files via upload

---
+++ product-configserver/src/test/java/com/training/product_configserver/ProductConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.product_configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
